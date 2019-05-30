334d7419657236dfd7d2f845dec2889937f00f6d
dodie91 <donnachaholmes1@gmail.com>
2019-05-30 21:59:53 +0100

    Added parentheses to print statements

--- Three and Five Test/threeandfive.py
+++ Three and Five Test/threeandfive.py
@@ -5,28 +5,28 @@ def simple():
     for i in range(1, 101):
         # If the condition of the int being divisible by both 3 and 5 is met - we print ThreeFive
         if i % 3 == 0 and i % 5 == 0:
-            print 'ThreeFive',
+            print('ThreeFive'),
         # If the condition of the int being divisible by 3 is met - we print Three;
         elif i % 3 == 0:
-            print 'Three', ;
+            print('Three'), ;
         # If the condition of the int being divisible by 5 is met - we print Five;
         elif i % 5 == 0:
-            print 'Five', ;
+            print('Five'), ;
         # Otherwise - We just print the integer as it is not divisible by either three or 5
         else:
-            print i, ;
+            print(i), ;
 
         # I use a count of 10 per line before moving to a new line just so it looks a bit nicer in the terminal
         count += 1
         if count == 10:
             count = 0
-            print'\n', ;
+            print('\n'), ;
 
 
 # A bit of a nicer way of doing it is to define a function that takes two arguments and displays this as needed
 def nicer_way(lower_limit, upper_limit):
     # Let the user know the range of numbers being examined
-    print'Displaying all the digits in the range {} to {}.\n'.format(lower_limit, upper_limit)
+    print('Displaying all the digits in the range {} to {}.\n'.format(lower_limit, upper_limit))
     upper_limit += 1
     # I create an empty list to add the values to before printing
     string_list = []
@@ -59,7 +59,7 @@ def nicer_way(lower_limit, upper_limit):
 # Call the simple way of doing this task
 simple()
 
-print'\n'
+print('\n')
 print('**** Function that takes two arguments ****')
 # Call the nicer way with the number range given in the task
 print(nicer_way(1, 100))
