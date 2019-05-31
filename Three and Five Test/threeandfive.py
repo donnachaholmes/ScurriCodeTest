b45390d49afbc5fbe24da00c634f945aba9ba6fb
Donnacha Holmes <donnachaholmes1@gmail.com>
2019-05-31 18:27:53 +0100

    Added comments to explain idea behind both solutions
    Signed-off-by: Donnacha Holmes <donnachaholmes1@gmail.com>

--- Three and Five Test/threeandfive.py
+++ Three and Five Test/threeandfive.py
@@ -1,3 +1,9 @@
+# The first solution to the problem goes through a range of numbers 1-100 and tests if the conditions for Three, Five
+# and ThreeFive are met
+# There is a variable that assigns the test number as a string - if one of the conditions are met, this changes to
+# Three, Five or ThreeFive
+# At the end of each loop we print the value of the num variable
+# if 10 numbers have been examined, we go to a new line
 for test_num in range(1, 101):
     num = str(test_num)
     if test_num % 3 == 0 and test_num % 5 == 0:
@@ -14,6 +20,8 @@ for test_num in range(1, 101):
 
 print('\n')
 
+# The second solution works on the same principles as the first solution except we use a list and add the value to the
+# list at the end of each cycle of the loop - Then after the loop has finished, we join the list with a space and print
 string_list = []
 for test_num in range(1, 101):
     num = str(test_num) + ' '
