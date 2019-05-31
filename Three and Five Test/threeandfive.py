bc15433eb2cb1310ffb7f8aab473af6c3e9a2a49
Donnacha Holmes <donnachaholmes1@gmail.com>
2019-05-31 18:15:07 +0100

    Removed function for second method and left just as a script to run - also removed the function call Changed method of how the second solution adds to the list removing an if statement added print statement to join the list and print
    Signed-off-by: Donnacha Holmes <donnachaholmes1@gmail.com>

--- Three and Five Test/threeandfive.py
+++ Three and Five Test/threeandfive.py
@@ -1,36 +1,31 @@
-for i in range(1, 101):
-    num = str(i)
-    if i % 3 == 0 and i % 5 == 0:
+for test_num in range(1, 101):
+    num = str(test_num)
+    if test_num % 3 == 0 and test_num % 5 == 0:
         num = 'ThreeFive'
-    elif i % 3 == 0:
+    elif test_num % 3 == 0:
         num = 'Three'
-    elif i % 5 == 0:
+    elif test_num % 5 == 0:
         num = 'Five'
 
     print(num, end=' ')
 
-    if i % 10 == 0:
+    if test_num % 10 == 0:
         print('\n', end='')
 
-
-def nicer_way(lower_limit, upper_limit):
-    upper_limit += 1
-    string_list = []
-    for test_num in range(lower_limit, upper_limit):
-        if test_num % 3 == 0 and test_num % 5 == 0:
-            string_list.append('ThreeFive ')
-        elif test_num % 3 == 0:
-            string_list.append('Three ')
-        elif test_num % 5 == 0:
-            string_list.append("Five ")
-        else:
-            string_list.append(str(test_num) + " ")
-
-        if test_num % 10 == 0:
-            string_list.append('\n')
-
-    return "".join(string_list)
-
-
 print('\n')
-print(nicer_way(1, 100))
+
+string_list = []
+for test_num in range(1, 101):
+    num = str(test_num) + ' '
+    if test_num % 3 == 0 and test_num % 5 == 0:
+        num = 'ThreeFive'
+    elif test_num % 3 == 0:
+        num = 'Three'
+    elif test_num % 5 == 0:
+        num = 'Five'
+    string_list.append(num)
+
+    if test_num % 10 == 0:
+        string_list.append('\n')
+
+print(''.join(string_list))
