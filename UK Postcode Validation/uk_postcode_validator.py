a5d2a24441e9308e1ebc05a4a1a514dab413b2f2
Donnacha Holmes <donnachaholmes1@gmail.com>
2019-06-10 18:40:38 +0100

    Changed how postcode is sent for validation, now sent with validate method
    Signed-off-by: Donnacha Holmes <donnachaholmes1@gmail.com>

--- UK Postcode Validation/uk_postcode_validator.py
+++ UK Postcode Validation/uk_postcode_validator.py
@@ -2,36 +2,35 @@ from character_validation import CharacterValidator
 
 
 class UKPostcodeValidation:
-    # We normalise the data by putting it in the upper case
-    # format and remove any whitespace for data processing
-    def __init__(self, postcode):
-        self.postcode = postcode.upper()
-        self.postcode = "".join(self.postcode.split())
 
     # The first stage of validation is validating the length of the postcode
     # There can only be 5 - 8 characters
-    def _validate_postcode_length(self):
-        length = len(self.postcode)
+    def _validate_postcode_length(self, postcode):
+        length = len(postcode)
         if length > 7 or length < 5:
             raise ValueError("Invalid Postcode")
         return True
 
     # The validate method used by the user - returns True or 'Invalid Postcode'
-    def validate(self):
-        validate_characters = CharacterValidator(self.postcode)
+    def validate(self, postcode):
+        postcode = postcode.upper()
+        postcode = "".join(postcode.split())
+        validate_characters = CharacterValidator(postcode)
         try:
-            if self._validate_postcode_length() and \
+            if self._validate_postcode_length(postcode) and \
                     validate_characters.validate_postcode_entries():
                 return True
         except ValueError as err:
             return err
 
     # The format method returns the postcode in the proper format if valid
-    def format(self):
+    def format(self, postcode):
+        postcode = postcode.upper()
+        postcode = "".join(postcode.split())
         try:
-            if self._validate_postcode_length() and self.validate():
-                outward_code = self.postcode[:len(self.postcode) - 3]
-                inward_code = self.postcode[-3::]
+            if self._validate_postcode_length(postcode) and self.validate():
+                outward_code = postcode[:len(postcode) - 3]
+                inward_code = postcode[-3::]
                 return outward_code + " " + inward_code
         except ValueError:
             return "Cannot format due to invalid postcode"
