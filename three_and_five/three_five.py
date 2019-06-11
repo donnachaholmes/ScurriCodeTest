b920617fc6376e5d62617489319e04cfa0097932
Donnacha Holmes <donnachaholmes1@gmail.com>
2019-06-11 08:58:50 +0100

    Changed to use __main__ and __name__
    Signed-off-by: Donnacha Holmes <donnachaholmes1@gmail.com>

--- three_and_five/three_five.py
+++ three_and_five/three_five.py
@@ -1,10 +1,15 @@
-for test_num in range(1, 101):
-    num = ""
-    if test_num % 3 == 0:
-        num += 'Three'
-    if test_num % 5 == 0:
-        num += 'Five'
-    if num == "":
-        num = str(test_num)
+def three_five():
+    for test_num in range(1, 101):
+        num = ""
+        if test_num % 3 == 0:
+            num += 'Three'
+        if test_num % 5 == 0:
+            num += 'Five'
+        if num == "":
+            num = str(test_num)
 
-    print(num, end=' ')
+        print(num, end=' ')
+
+
+if __name__ == "__main__":
+    three_five()
